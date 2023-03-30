4cc83f009240810631de369b4bd776f29dc83786
chaturvedi-anil <chaturvedia560@gmail.com>
2023-03-30 21:14:08 +0530

    rendering page using ejs

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ const app=express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+// --------------controlers-------------->
 app.get('/', function(req, res)
 {
     // console.log(__dirname);
     // res.send('<h1>Its running now</h1>');
-    return res.render('home');
+    return res.render('home', {title: "Home Ejs"} );
 });
+app.get('/practice', function(req, res)
+{
+    return res.render('practice', {title: "Practice page"});
+});
+
+
 
 app.listen(port, function(err)
 {
