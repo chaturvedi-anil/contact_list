5e4c89d3e6255f1ccb5f9432cb12df3938309d49
chaturvedi-anil <chaturvedia560@gmail.com>
2023-04-04 15:32:32 +0530

    mongodb connected

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+//require mongoose library
+const mongoose=require('mongoose');
+
+// connect to database 
+mongoose.connect('mongodb://localhost/contact_list_db');
+
+// aquire th connection (to check if it is succesful)
+const db=mongoose.connection;
+
+//error
+db.on('error', console.error.bind(console, 'error connecting to db'));
+
+// up and running then print the message
+db.once('open', function()
+{
+    console.log("Successfully connected to database");
+});
